107b2312e4a3c824cd9a7b6c5d8be0242b38a739
PyroPM <sean.k.mapes@gmail.com>
2025-02-25 22:22:08 -0500

    add extension capability

--- 5e/redwood/scripts.js
+++ 5e/redwood/scripts.js
@@ -1,6 +1,4 @@
 
-let backgroundPage = browser.extension.getBackgroundPage();
-
 var character = "Hegbast Redwood";
 
 var attack = document.getElementsByClassName("attack");
@@ -9,12 +7,18 @@ var damage = document.getElementsByClassName("damage");
 
 for (let i = 0; i < to_hit.length; i++) {
     to_hit[i].addEventListener("click", function(e) {
-        backgroundPage.chat(`&{template:default} {{name=${attack[i].innerText}}} {{To Hit=[[1d20${to_hit[i].innerText}]]}} {{Damage=[[${damage[i].innerText}]]}}`);
+        window.postMessage({
+            type: "custom20",
+            message: `&{template:default} {{name=${attack[i].innerText}}} {{To Hit=[[1d20${to_hit[i].innerText}]]}} {{Damage=[[${damage[i].innerText}]]}}`,
+        })
     });
 }
 
 for (let i = 0; i < to_hit.length; i++) {
     damage[i].addEventListener("click", function(e) {
-        backgroundPage.chat(`&{template:default} {{name=${attack[i].innerText}}} {{To Hit=[[1d20${to_hit[i].innerText}]]}} {{Damage=[[${damage[i].innerText}]]}}`);
+        window.postMessage({
+            type: "custom20",
+            message: `&{template:default} {{name=${attack[i].innerText}}} {{To Hit=[[1d20${to_hit[i].innerText}]]}} {{Damage=[[${damage[i].innerText}]]}}`,
+        })
     });
 }
