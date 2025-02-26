366aaa9147a022c1d4988cb6a72e0018a9a4ec49
PyroPM <sean.k.mapes@gmail.com>
2025-02-25 21:39:17 -0500

    initial commit

---
+++ 5e/redwood/scripts.js
@@ -0,0 +1,20 @@
+
+let backgroundPage = browser.extension.getBackgroundPage();
+
+var character = "Hegbast Redwood";
+
+var attack = document.getElementsByClassName("attack");
+var to_hit = document.getElementsByClassName("to_hit");
+var damage = document.getElementsByClassName("damage");
+
+for (let i = 0; i < to_hit.length; i++) {
+    to_hit[i].addEventListener("click", function(e) {
+        backgroundPage.chat(`&{template:default} {{name=${attack[i].innerText}}} {{To Hit=[[1d20${to_hit[i].innerText}]]}} {{Damage=[[${damage[i].innerText}]]}}`);
+    });
+}
+
+for (let i = 0; i < to_hit.length; i++) {
+    damage[i].addEventListener("click", function(e) {
+        backgroundPage.chat(`&{template:default} {{name=${attack[i].innerText}}} {{To Hit=[[1d20${to_hit[i].innerText}]]}} {{Damage=[[${damage[i].innerText}]]}}`);
+    });
+}
