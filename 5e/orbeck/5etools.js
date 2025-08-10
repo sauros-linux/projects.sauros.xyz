9654e2a1840b4c172afa84ccb013c51a8b969b60
PyroPM <sean.k.mapes@gmail.com>
2025-08-10 15:31:32 -0400

    add new spell lookup

--- 5e/orbeck/5etools.js
+++ 5e/orbeck/5etools.js
@@ -1,7 +1,30 @@
 
-async function init() {
-    let spells = await get_data("https://5e.tools/data/spells/index.json");
-    console.log(spells);
+async function get_data(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+    
+        const json = await response.json();
+        return json;
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
-init();
+async function get_spell(source, name) {
+    let data = await get_data(`https://5e.tools/data/spells/spells-${source.toLowerCase()}.json`);
+    if (data == null) {
+        return;
+    }
+
+    let spell_result = data["spell"].filter((spell) => spell.name == name);
+
+    console.log(spell_result);
+    if (spell_result.length > 1) {
+        const SPELL = spell_result[0];
+    } else {
+        console.log("got multiple spells!");
+    }
+}
